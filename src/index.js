2646f1c93f6b93f7edfb38a6a1f8589958284ffb
Valentave <valentintaverna99@gmail.com>
2023-06-24 02:59:55 -0300

    Add functionality to switch tabs
    Added the events to switch tabs when pressing the respective buttons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {generateFooter} from './generateFooter.js';
 import {generateMenu} from './generateMenu.js';
 import {generateAboutUs} from './generateAboutUs.js';
 import './style.css';
-import './nav.css';
+import './header.css';
 
 const body = document.querySelector('body');
 const content = document.createElement('div');
@@ -15,14 +15,49 @@ const main = document.createElement('main');
 main.classList.add('main');
 
 // Initialize the header
-content.appendChild(generateHeader());  
+const header = generateHeader();
+content.appendChild(header);  
 
 // Initialize the hero
 content.appendChild(main);
 
-main.appendChild(generateHero());
-main.appendChild(generateMenu());
-main.appendChild(generateAboutUs());
+const hero = generateHero();
+const menu = generateMenu();
+const aboutUs = generateAboutUs();
+main.appendChild(hero);
+main.appendChild(menu);
+main.appendChild(aboutUs);
 
 // Initialize the footer
-content.appendChild(generateFooter());
+const footer = generateFooter();
+content.appendChild(footer);
+
+// Initialize buttons
+const btnHome = header.lastChild.querySelector('.nav__home');
+const btnMenu = header.lastChild.querySelector('.nav__menu');
+const btnAboutUs = header.lastChild.querySelector('.nav__about-us');
+
+// Initialize events
+btnHome.addEventListener('click', () => {
+    if(hero.classList.contains('hidden')) {
+        hero.classList.remove('hidden');
+        if (!menu.classList.contains('hidden')) menu.classList.add('hidden');
+        if (!aboutUs.classList.contains('hidden')) aboutUs.classList.add('hidden');
+    }
+})
+
+btnMenu.addEventListener('click', () => {
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        if (!hero.classList.contains('hidden')) hero.classList.add('hidden');
+        if (!aboutUs.classList.contains('hidden')) aboutUs.classList.add('hidden');
+    }
+})
+
+btnAboutUs.addEventListener('click', () => {
+    if(aboutUs.classList.contains('hidden')) {
+        aboutUs.classList.remove('hidden');
+        if (!hero.classList.contains('hidden')) hero.classList.add('hidden');
+        if (!menu.classList.contains('hidden')) menu.classList.add('hidden');
+    }
+})
